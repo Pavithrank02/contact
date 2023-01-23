2bb1dc2ffed788d0d9efd7362205c6145c5942dc
Pavithran2 <pavithrank02@gmail.com>
2023-01-23 19:46:09 +0530

    navigation routing checked

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import './App.css';
+import Navbar from './components/Navbar';
 
-function App() {
+const  App = () => {
   return (
     <div className="App">
-      
+      <ToastContainer />
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={ <h1>iam an component</h1>}>
+         
+        </Route>
+        <Route exact path="/add" element={ <h1>I am Add Component</h1>}>
+           
+        </Route>
+        <Route  path="/edit/:id">
+           Edit Component
+        </Route>
+      </Routes>
     </div>
   );
 }
